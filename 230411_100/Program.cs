4ef878cfb04aac4155419541838572d5934d1586
simgksk <Daum_c0m@naver.com>
2023-04-11 22:38:25 +0900

    초기 커밋.

---
+++ 230411_100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _230411_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<=n; i++)
+            {   
+                for(int j=n; j>i; j--)
+                {
+                    Console.Write("*");
+                }
+                    Console.WriteLine();
+            }
+           
+
+        }
+    }
+}
